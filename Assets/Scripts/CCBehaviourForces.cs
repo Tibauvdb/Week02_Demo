9dd4d543f7290306a76075050202ef3fceca5753
Tibau vdb <tibau.van.den.broeck@student.howest.be>
2018-10-04 12:31:08 +0200

    Added Forces and CC using forces

---
+++ Assets/Scripts/CCBehaviourForces.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCBehaviourForces : MonoBehaviour {
+    private Rigidbody _rb;
+
+    [SerializeField]
+    private float _acceleration;
+
+    private bool _jump;
+
+    [SerializeField]
+    private float _jumpHeight;
+	// Use this for initialization
+	void Start () {
+        _rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            _jump = true;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        Movement();
+        Jump();
+    }
+    private void Movement()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        _rb.AddForce(input * _acceleration, ForceMode.Impulse);
+    }
+
+    private void Jump()
+    {
+        if (_jump && this.gameObject.transform.position.y <=0)
+        {
+            _rb.AddForce(Vector3.up * _rb.mass * _jumpHeight, ForceMode.Impulse);
+            _jump = false;
+        }
+    }
+
+    private void LimitMaxVelocity()
+    {
+
+    }
+
+ 
+}
---
+++ Assets/Scripts/DemoAddForceBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoAddForceBehaviour : MonoBehaviour {
+
+    private Rigidbody _rb;
+    [SerializeField]
+    private float _acceleration;
+    private bool _jump;
+	// Use this for initialization
+	void Start () {
+        _rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            _jump = true;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if (_jump)
+        {
+            _rb.AddForce(Vector3.up *  _rb.mass * _acceleration,ForceMode.Impulse);
+            _jump = false;
+        }
+    }
+}
